56e6d658e1b8bd6d861d33c2c34db23228ba744f
Maurice Barnes <mauriceb2197@gmail.com>
2019-08-22 22:22:39 -0400

    Added a example io channel

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import * as express from 'express';
 import {Response, Request} from 'express';
+import * as socketio from 'socket.io';
+
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req: Request, res: Response) => res.send('Hello World'));
 
+let http = require("http").Server(app);
+let io = require("socket.io")(http);
 
+
+io.on("connection", (socket: any) => {
+  console.log("a user connected");
+});
 app.listen(port, () => console.log('Server is listening'));
